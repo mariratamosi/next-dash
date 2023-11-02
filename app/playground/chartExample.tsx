c90a7fffa3de2074b0a4135b082e0361e3a9e239
mariratamosi <rongrazzo@gmail.com>
2023-11-02 21:26:41 +0600

    added __

---
+++ app/playground/chartExample.tsx
@@ -0,0 +1,22 @@
+import { Card, Grid, Metric, Text } from "@tremor/react";
+
+const categories = [
+  {
+    title: "Sales",
+    metric: "$ 23,456",
+  },
+  {
+    title: "Profit",
+    metric: "$ 13,123",
+  },
+  {
+    title: "Customers",
+    metric: "456",
+  },
+];
+
+export default function ChartExample() {
+  return (
+    <h1>Chart EXAMPLE SHOULD HAVE BEEN HERE</h1>
+  );
+}
